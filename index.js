951379ba98cb380cd24989880eb6e019e5e09eb1
Đoàn Đức Bảo <psawn@users.noreply.github.com>
2020-08-05 20:32:12 +0700

    sua code project nodejs

--- index.js
+++ index.js
@@ -9,23 +9,11 @@ app.listen(PORT,function () {
 app.use(express.static("public"));
 app.set("view engine","ejs");
 var counter = 0;
-app.get("/",function (req,res) {
-    //res.sendFile(__dirname+"/views/answer_assignment13.html");
-    let title ="Du bao thoi tiet";
-    counter++;
-    res.render("answer_assignment13",{
-        title:title,
-        counter:counter,
-    });
-});
 
-app.get("/login",function (req,res) {
-    res.send("Day la trang login");
-});
 const fs = require("fs");
 
 
-app.get("/project",function (req,res) {
-    res.render("home-project")
+app.get("/",function (req,res) {
+    res.render("home-project");
 })
 
