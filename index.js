69289b0d4d1a9ff9e2b8dd68aa3b3c5c760c233f
Đoàn Đức Bảo <psawn@users.noreply.github.com>
2020-08-05 20:22:19 +0700

    sua code project nodejs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require ("express");
 const app = express();
-const PORT = process.env.PORT || 6060;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,function () {
     console.log("Sever is running");
