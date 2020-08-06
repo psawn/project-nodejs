0beef30f22ffe3c4820c89a401100617edfbf060
Đoàn Đức Bảo <psawn@users.noreply.github.com>
2020-08-06 08:25:06 +0700

    them about + code nodejs blogpage

--- index.js
+++ index.js
@@ -12,8 +12,35 @@ var counter = 0;
 
 const fs = require("fs");
 
-
 app.get("/",function (req,res) {
     res.render("home-project");
 })
 
+// code blogpage
+app.get("/blogpage",function (req,res) {
+    let cats=fs.readFileSync("data/blogdt.json","UTF-8");
+    cats=JSON.parse(cats);
+    res.render("blogpage",{
+        cats : cats,
+        id : 1
+    });
+})
+app.get("/blogpage/:pg",function (req,res) {
+    let cats=fs.readFileSync("data/blogdt.json","UTF-8");
+    cats=JSON.parse(cats);
+    let p = req.params.pg;
+    let count = 0
+    cats.map(e=>{
+        count++;
+        if((cats.length-e.num+3)/3 == p){
+            res.render("blogpage",{
+                cats : cats,
+                id : parseInt(p)
+            });
+            count = 0;
+        };
+    });
+    if(count>=cats.length){
+        res.send("Khong tim thay");
+    };
+})
